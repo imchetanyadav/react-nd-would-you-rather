5ce628b089e07adafb2c2d1e008942c465723391
Chetan Yadav <imchetanyadav@gmail.com>
2018-08-29 05:30:18 +0530

    Added loading bar

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,4 +1,5 @@
 import { combineReducers } from 'redux'
+import { loadingBarReducer } from 'react-redux-loading'
 import authedUser from './authedUser'
 import users from './users'
 import questions from './questions'
@@ -6,5 +7,6 @@ import questions from './questions'
 export default combineReducers({
     authedUser,
     users,
-    questions
+    questions,
+    loadingBar: loadingBarReducer
 })
