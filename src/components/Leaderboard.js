42049e87698c80714fd2bfd7a89192bcbe169371
Chetan Yadav <imchetanyadav@gmail.com>
2018-08-30 12:04:26 +0530

    Added leaderboard data logic

--- src/components/Leaderboard.js
+++ src/components/Leaderboard.js
@@ -1,13 +1,50 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
 
 class Leaderboard extends Component {
     render() {
+        const { leaderboardData } = this.props
+
         return (
             <div>
                 Leaderboard component
+                {leaderboardData ? 
+                    leaderboardData.map(user => (
+                        <div key={user.name}>
+                            {user.name}
+                            <li>
+                                Answered Questions: {user.answeredQuestions}
+                            </li>
+                            <li>
+                                Created Questions: {user.createdQuestions}
+                            </li>
+                            <li>
+                                Total: {user.answeredQuestions+user.createdQuestions}
+                            </li>
+                            <hr />
+                        </div>
+                    ))
+                : null
+                }
             </div>
         )
     }
 }
 
-export default Leaderboard
+
+function mapStateToProps ({ authedUser, users, questions }) {
+    const leaderboardData = Object.keys(users).map(user => ({
+        name: users[user].name,
+        avatarURL: users[user].avatarURL,
+        answeredQuestions: Object.keys(users[user].answers).length,
+        createdQuestions: Object.keys(questions).filter(q => questions[q].author === user).length
+    })).sort((a,b) => (b.answeredQuestions+b.createdQuestions)-(a.answeredQuestions+a.createdQuestions))
+
+    return {
+        authedUser,
+        leaderboardData
+    }
+}
+
+
+export default connect(mapStateToProps)(Leaderboard)
