9fc2f368581fe4d43aa41a4d7cdf7d7ea630684b
Chetan Yadav <imchetanyadav@gmail.com>
2018-08-30 12:47:34 +0530

    Added logout functionality

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,29 +1,40 @@
-import React from 'react'
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
 import { NavLink } from 'react-router-dom'
+import { setAuthedUser } from '../actions/authedUser'
 
-export default function Navbar(props) {
-    return (
-        <nav>
-            <ul>
-                <li>
-                    <NavLink to='/' exact activeClassName='active'>
-                        Home
-                    </NavLink>
-                </li>
-                <li>
-                    <NavLink to='/add' exact activeClassName='active'>
-                        New Question
-                    </NavLink>
-                </li>
-                <li>
-                    <NavLink to='/leaderboard' exact activeClassName='active'>
-                        Leaderboard
-                    </NavLink>
-                </li>
-            </ul>
-            <span>
-                {props.authedUser}
-            </span>
-        </nav>
-    )
+class Navbar extends Component {
+    handleLogout = () => {
+        const { dispatch } = this.props
+        dispatch(setAuthedUser(null))
+    }
+    render() {
+        return (
+            <nav>
+                <ul>
+                    <li>
+                        <NavLink to='/' exact activeClassName='active'>
+                            Home
+                        </NavLink>
+                    </li>
+                    <li>
+                        <NavLink to='/add' exact activeClassName='active'>
+                            New Question
+                        </NavLink>
+                    </li>
+                    <li>
+                        <NavLink to='/leaderboard' exact activeClassName='active'>
+                            Leaderboard
+                        </NavLink>
+                    </li>
+                </ul>
+                <span>
+                    {this.props.authedUser}
+                    <button onClick={this.handleLogout}>Logout</button>
+                </span>
+            </nav>
+        )
+    }
 }
+
+export default connect()(Navbar)
